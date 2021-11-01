382fe02555859b55a20556d3c833adba9efba65d
Misha Martin <mishanicolemartin@gmail.com>
2021-11-01 14:06:15 -0600

    Added submit endpoint

--- client/main.js
+++ client/main.js
@@ -1,3 +1,26 @@
 console.log("Hello World!")
 
-const affirmation = document.getElementById("affirmation")
+const baseUrl = "https://i-am-affirmations.herokuapp.com"
+
+const affirmationBtn = document.getElementById("affirmation")
+const addAffirmation = document.getElementById("addAffirmation")
+const addBtn = document.getElementById("addBtn")
+const themes = document.getElementById("themes")
+const addForm = document.querySelector("form")
+
+function addAffirmation(event) {
+    event.preventDefault()
+
+    let body = {
+        affirmation: affirmation.value
+    }
+    
+    axios.post(`${baseUrl}/add`, body)
+    .then(res => {
+        console.log(res.data)
+    })
+    .catch(err => console.log(err))
+}
+
+addForm.addEventListener('submit', addAffirmation)
+
