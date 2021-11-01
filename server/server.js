df7cc9e6381422f90c483b68b48da420889ca346
Misha Martin <mishanicolemartin@gmail.com>
2021-11-01 12:05:40 -0600

    added rollbar and heroku

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '141762b8a510497c802e7f2c1e60b5a6',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
 
 const app = express()
 
@@ -7,6 +18,8 @@ app.use(express.json())
 const ctrl = require("./controller.js")
 
 //endpoints
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
